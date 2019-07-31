44680525b34514e46b9a53ba63ac3f79f52c8e3d
lenar <lguitar@mail.ru>
2019-07-31 14:42:51 +0300

    update

--- src/Animator.js
+++ src/Animator.js
@@ -1,137 +1,133 @@
-import React, { Component } from "react";
-import { PanResponder, Animated, Dimensions, StyleSheet } from "react-native";
-import { DOWN_STATE, UP_STATE } from "./BottomDrawer";
+import React, {Component} from 'react';
+import {PanResponder, Animated, Dimensions, StyleSheet} from 'react-native';
+import {DOWN_STATE, UP_STATE} from "./BottomDrawer";
 
-const SCREEN_HEIGHT = Dimensions.get("window").height;
-const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default class Animator extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.position = new Animated.ValueXY(this.props.currentPosition);
+        this.position = new Animated.ValueXY(this.props.currentPosition);
 
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => false,
-      onPanResponderMove: this._handlePanResponderMove,
-      onPanResponderRelease: this._handlePanResponderRelease
-    });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.drawerState !== this.props.drawerState) {
-      if (nextProps.drawerState === 0) {
-        this._transitionTo(this.props.downPosition, this.props.onCollapsed);
-      }
-      if (nextProps.drawerState === 1) {
-        this._transitionTo(this.props.upPosition, this.props.onExpanded);
-      }
+        this._panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: this._handlePanResponderMove,
+            onPanResponderRelease: this._handlePanResponderRelease
+        });
     }
-  }
 
-  render() {
-    return (
-      <Animated.View
-        style={[
-          this.props.style,
-          { ...this.position.getLayout(), left: 0 },
-          StyleSheet.flatten([
-            styles.animationContainer(
-              this.props.containerHeight,
-              this.props.backgroundColor
-            ),
-            styles.roundedEdges(this.props.roundedEdges),
-            styles.shadow(this.props.shadow)
-          ])
-        ]}
-        {...this._panResponder.panHandlers}
-      >
-        {this.props.children}
-      </Animated.View>
-    );
-  }
-
-  _handlePanResponderMove = (e, gesture) => {
-    if (this._swipeInBounds(gesture)) {
-      this.position.setValue({ y: this.props.currentPosition.y + gesture.dy });
-    } else {
-      this.position.setValue({
-        y: this.props.upPosition.y - this._calculateEase(gesture)
-      });
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.drawerState !== this.props.drawerState) {
+            if (nextProps.drawerState === 0) {
+                this._transitionTo(this.props.downPosition, this.props.onCollapsed);
+            }
+            if (nextProps.drawerState === 1) {
+                this._transitionTo(this.props.upPosition, this.props.onExpanded);
+            }
+        }
     }
-  };
 
-  _handlePanResponderRelease = (e, gesture) => {
-    if (
-      gesture.dy > this.props.toggleThreshold &&
-      this.props.currentPosition === this.props.upPosition
-    ) {
-      this._transitionTo(this.props.downPosition, this.props.onCollapsed);
-      this.props.onDrawerStateSet(DOWN_STATE);
-    } else if (
-      gesture.dy < -this.props.toggleThreshold &&
-      this.props.currentPosition === this.props.downPosition
-    ) {
-      this._transitionTo(this.props.upPosition, this.props.onExpanded);
-      this.props.onDrawerStateSet(UP_STATE);
-    } else {
-      this._resetPosition();
+    render() {
+        return (
+            <Animated.View
+                style={[
+                    this.props.style,
+                    {...this.position.getLayout(), left: 0},
+                    StyleSheet.flatten([
+                        styles.animationContainer(
+                            this.props.containerHeight,
+                            this.props.backgroundColor
+                        ),
+                        styles.roundedEdges(this.props.roundedEdges),
+                        styles.shadow(this.props.shadow)
+                    ])
+                ]}
+                {...this._panResponder.panHandlers}
+            >
+                {this.props.children}
+            </Animated.View>
+        );
     }
-  };
 
-  // returns true if the swipe is within the height of the drawer.
-  _swipeInBounds(gesture) {
-    return this.props.currentPosition.y + gesture.dy > this.props.upPosition.y;
-  }
+    _handlePanResponderMove = (e, gesture) => {
+        if (this._swipeInBounds(gesture)) {
+            this.position.setValue({y: this.props.currentPosition.y + gesture.dy});
+        } else {
+            this.position.setValue({
+                y: this.props.upPosition.y - this._calculateEase(gesture)
+            });
+        }
+    };
 
-  _calculateEase(gesture) {
-    return Math.min(Math.sqrt(gesture.dy * -1), Math.sqrt(SCREEN_HEIGHT));
-  }
+    _handlePanResponderRelease = (e, gesture) => {
+        if (
+            gesture.dy > this.props.toggleThreshold &&
+            this.props.currentPosition.y === this.props.upPosition.y
+        ) {
+            this._transitionTo(this.props.downPosition, this.props.onCollapsed);
+            this.props.onDrawerStateSet(DOWN_STATE);
+        } else if (
+            gesture.dy < -this.props.toggleThreshold &&
+            this.props.currentPosition === this.props.downPosition
+        ) {
+            this._transitionTo(this.props.upPosition, this.props.onExpanded);
+            this.props.onDrawerStateSet(UP_STATE);
+        } else {
+            this._resetPosition();
+        }
+    };
 
-  _transitionTo(position, callback) {
-    Animated.timing(this.position, {
-      toValue: position
-    }).start(() => callback());
+    // returns true if the swipe is within the height of the drawer.
+    _swipeInBounds(gesture) {
+        return this.props.currentPosition.y + gesture.dy > this.props.upPosition.y;
+    }
 
-    this.props.setCurrentPosition(position);
-    callback();
-  }
+    _calculateEase(gesture) {
+        return Math.min(Math.sqrt(gesture.dy * -1), Math.sqrt(SCREEN_HEIGHT));
+    }
 
-  _resetPosition() {
-    Animated.timing(this.position, {
-      toValue: this.props.currentPosition
-    }).start();
-  }
+    _transitionTo(position, callback) {
+        Animated.timing(this.position, {
+            toValue: position
+        }).start(() => callback());
+
+        this.props.setCurrentPosition(position);
+        callback();
+    }
+
+    _resetPosition() {
+        Animated.timing(this.position, {
+            toValue: this.props.currentPosition
+        }).start();
+    }
 }
 
 const styles = {
-  animationContainer: (height, color) => ({
-    width: SCREEN_WIDTH,
-    position: "absolute",
-    height: height + Math.sqrt(SCREEN_HEIGHT),
-    backgroundColor: color
-  }),
-  roundedEdges: rounded => {
-    return (
-      rounded == true && {
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20
-      }
-    );
-  },
-  shadow: shadow => {
-    return (
-      shadow == true && {
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-
-        elevation: 4,
-      }
-    );
-  }
-};
+    animationContainer: (height, color) => ({
+        width: SCREEN_WIDTH,
+        position: 'absolute',
+        height: height + Math.sqrt(SCREEN_HEIGHT),
+        backgroundColor: color,
+        zIndex: 100,
+    }),
+    roundedEdges: rounded => {
+        return rounded == true && {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+        }
+    },
+    shadow: shadow => {
+        return shadow == true && {
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4,
+        }
+    },
+}
